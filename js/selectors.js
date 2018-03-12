0de21cc01f7df876e7e271880ed7abf35311d159
Richard Fenix <richard.fenix@gmail.com>
2018-03-12 15:32:16 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -20,3 +20,12 @@ function divSelector(){
 function firstListItem (){
   return $('ul li:first-child')
 }
+
+/* problem
+Write a function firstListItem that does not accept any parameters. The function should use a first-child selector to return the first list item in the ul with the ID pic-list.
+
+solution
+function firstListItem(){
+  return $('ul#pic-list li:first-child');
+}
+*/
